895079ef254ad6bc962a9d29ed265db4a77bca46
manishchandrajwar <80349777+manishchandrajwar@users.noreply.github.com>
2021-06-17 12:30:58 +0530

    Added file in src

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import StateDistrict from '../src/Components/StateDistrict'
+
+
+function App() {
+  return (
+    <div>
+      <StateDistrict/>
+    </div>
+  );
+}
+
+export default App;
